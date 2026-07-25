1f8637cfdc975291f8740e51f01053b4931b01a8
Kushagra Nayak <Kushagranayak55@gmail.com>
2026-07-26 03:06:54 +0530

    Configure production API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 // Backend is assumed to already exist and run here.
-const BASE_URL = 'http://localhost:8000'
+const BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
 
 const client = axios.create({
   baseURL: BASE_URL,
